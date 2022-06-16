2181e14c8b3974e65958c9812d4a3f2061c252b9
Gabriel <omnifarter@gmail.com>
2022-06-16 23:25:37 +0800

    add config

--- travels/config.js
+++ travels/config.js
@@ -1,4 +1,4 @@
-{
+var config = {
     "style": "mapbox://styles/omnifarter/cl4h24ssw000b15jufkwb8e3m",
     "accessToken": "pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg",
     "showMarkers": false,
@@ -9,12 +9,12 @@
     "chapters": [
         {
             "id": "summary",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Summary",
             "countries": 12,
             "cities":1,
             "location": {
-                "center": [32.061,68.924],
+                "center": [68.924,32.061],
                 "zoom": 2.25,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -22,7 +22,7 @@
             "onChapterEnter": [
                 {
                     "layer": "country-boundaries",
-                    "opacity": 1
+                    "opacity": 0.4
                 }
             ],
             "onChapterExit": [
@@ -34,14 +34,14 @@
         },
         {
             "id": "france",
-            "alignment": "full",
+            "alignment": "left",
             "title": "France Pre-Graduation Trip",
             "days":13,
             "date":"April 2022",
-            "image":"",
+            "image":"https://storage.googleapis.com/omnifarter-travel/france.jpg",
             "places": ["Paris","Reims","Tours","Bordaeux","Arcachon","Dorgdogne Valley","Clermont-Ferrand","Annecy","Chamonix","Nice"],
             "location": {
-                "center": [-46.483,2.484],
+                "center": [2.48383, 46.48293],
                 "zoom": 5.89,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -61,14 +61,14 @@
         },
         {
             "id": "hangzhou",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Hangzhou Exchange Program",
             "image":"",
             "days":100,
             "date":"May 2019",
             "places": ["Hangzhou","Shanghai","Taizhou","Huzhou","Guilin","Zhangjiajie","Chengdu","Huangshan"],
             "location": {
-                "center": [28.446,110.594],
+                "center": [110.594,28.446],
                 "zoom": 5.92,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -88,14 +88,14 @@
         },
         {
             "id": "taiwan-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Taiwan Family Trip",
             "image":"",
             "days":9,
             "date":"Dec 2018",
             "places": ["Taipei","Pingxi","Taroko Gorge","Hualien"],
             "location": {
-                "center": [24.546,121.382],
+                "center": [121.382,24.546],
                 "zoom": 9.07,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -115,14 +115,14 @@
         },
         {
             "id": "tioman-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Tioman Diving Trip",
             "image":"",
             "days":3,
             "date":"Aug 2018",
             "places": ["Tioman"],
             "location": {
-                "center": [2.792,104.091],
+                "center": [104.091,2.792],
                 "zoom": 11.90,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -142,14 +142,14 @@
         },
         {
             "id": "cameron-highland-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Cameron Highland Term Break Trip",
             "image":"",
             "days":3,
             "date":"Jun 2018",
             "places": ["Cameron Highlands"],
             "location": {
-                "center": [4.476,101.358],
+                "center": [101.358,4.476],
                 "zoom": 13.63,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -169,14 +169,14 @@
         },
         {
             "id": "bali-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Bali 2018",
             "image":"",
             "days":3,
             "date":"Apr 2018",
             "places": ["Kuta","Tanah Lot"],
             "location": {
-                "center": [-8.690,115.129],
+                "center": [115.129,-8.690],
                 "zoom": 11.97,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -196,14 +196,14 @@
         },
         {
             "id": "australia-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Australia 2018",
             "image":"",
             "days":9,
             "date":"Mar 2018",
             "places": ["Sydney","Hunter's Valley","Ellenborough Falls","Port Macquarie","Coff's Harbour","Gold Coast","Brisbane"],
             "location": {
-                "center": [-30.706,152.345],
+                "center": [152.345,-30.706],
                 "zoom": 6.43,
                 "pitch": 0.00,
                 "bearing": 0.00
@@ -223,14 +223,14 @@
         },
         {
             "id": "krabi-2018",
-            "alignment": "full",
+            "alignment": "left",
             "title": "Krabi ORD Trip 2018",
             "image":"",
             "days":6,
             "date":"Mar 2018",
             "places": ["Krabi","Ao Nang","Phi Phi Islands"],
             "location": {
-                "center": [7.893,98.703],
+                "center": [98.703,7.893],
                 "zoom": 11.06,
                 "pitch": 0.00,
                 "bearing": 0.00
